cda62a0ec340e5a41ec7eebc5642d35e4618685c
reinaldoams <reinaldoams@gmail.com>
2021-08-08 22:27:06 -0300

    testando juliofodao

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React com julio fodaaa!!
         </a>
       </header>
     </div>
