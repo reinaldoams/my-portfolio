02c45d83bac858b1fff4847336786b451e5cbc09
reinaldoams <reinaldoams@gmail.com>
2021-08-09 00:59:06 -0300

    segundo commit

---
+++ src/components/Project.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Project = () => {
+    return ( 
+        <h1>Project Page!</h1>
+     );
+}
+ 
+export default Project;
---
+++ src/components/SinglePost.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SinglePost = () => {
+    return ( 
+        <h1>SinglePost Page!</h1>
+     );
+}
+ 
+export default SinglePost;
