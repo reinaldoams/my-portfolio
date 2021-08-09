5fe2ceee66dbaac536338290ffa44203a93f7a4f
reinaldoams <reinaldoams@gmail.com>
2021-08-09 17:34:32 -0300

    antes da caminhadinha do segundo dia

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,9 @@ const Home = () => {
         <main>
             <img src={image} alt="Background Image" className="absolute object-cover w-full h-full" />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Aloha. I'm Kapehe.</h1>
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+                    Reinaldo Assis, Web Developer
+                    </h1>
             </section>
         </main>
     );
