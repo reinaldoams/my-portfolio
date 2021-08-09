2d0d65e1271dccc45133cf1dce09a073240cb410
reinaldoams <reinaldoams@gmail.com>
2021-08-09 02:26:14 -0300

    commit apos 4h morrendo

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
-import sanityClient from ‘@sanity/client’
+import sanityClient from '@sanity/client'
 
 export default sanityClient({
-projectId: “bowy488m”
-dataset: “production”
+projectId: "bowy488m",
+dataset: "production"
 })
